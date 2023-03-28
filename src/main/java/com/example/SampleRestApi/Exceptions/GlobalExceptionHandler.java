7db96278d876f2e7769f572a30eacd7ba9ec2961
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2023-03-28 18:13:46 +0530

    GlobalExceptionHandler changes

--- src/main/java/com/example/SampleRestApi/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/SampleRestApi/Exceptions/GlobalExceptionHandler.java
@@ -1,17 +1,21 @@
 package com.example.SampleRestApi.Exceptions;
 
 import com.example.SampleRestApi.DTO.ErrorDetails;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
     @ExceptionHandler(ConstraintException.class)
     public ResponseEntity<ErrorDetails> constraintException(ConstraintException exception, WebRequest webRequest){
         ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
@@ -24,15 +28,23 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
     }
 
-    @ExceptionHandler(APIException.class)
-    public ResponseEntity<ErrorDetails> APIException(APIException exception, WebRequest webRequest){
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
-        return new ResponseEntity<>(errorDetails, exception.getStatus());
-    }
+//    @ExceptionHandler(APIException.class)
+//    public ResponseEntity<ErrorDetails> handleAPIException(APIException exception, WebRequest webRequest) {
+//        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+//        return new ResponseEntity<>(errorDetails, exception.getStatus());
+//    }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorDetails> globalException(ConstraintException exception, WebRequest webRequest){
+    public ResponseEntity<ErrorDetails> globalException(Exception exception, WebRequest webRequest){
         ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        System.out.println(exception);
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+//    @ExceptionHandler(MalformedJwtException.class)
+//    public ResponseEntity<ErrorDetails> globalJWTException(Exception exception, WebRequest webRequest){
+//        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+//        System.out.println(exception);
+//        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
 }
